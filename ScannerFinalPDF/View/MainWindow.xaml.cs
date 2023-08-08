3851c40f21a8038ab3c76af35fd212bf8d0950d4
Mister Empty <mr.emptyboss@gmail.com>
2023-08-08 21:05:45 +0600

    Begin

---
+++ ScannerFinalPDF/View/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using ScannerFinalPDF.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScannerFinalPDF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainHome secondForm;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButt_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MinButt_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void ToolBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            secondForm = new MainHome();
+            secondForm.setCreatingForm = this;
+            secondForm.Show();
+            this.Close();
+        }
+    }
+}
---
+++ ScannerFinalPDF/ViewModel/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ScannerFinalPDF.ViewModel
+{
+    class MainViewModel : ViewModelBase
+    {
+        private Page Welcome;
+        private Page Profile;
+        private Page Settings;
+        private Page ControlPanel;
+
+        private Page _currentPage;
+
+       public Page CurrentPage
+        {
+            get => _currentPage;
+            set => Set(ref _currentPage, value);
+        }
+
+
+        public MainViewModel()
+        {
+            Welcome = new View.Pages.Welcome();
+            Profile = new View.Pages.Profile();
+            Settings = new View.Pages.Settings();
+            ControlPanel = new View.Pages.ControlPanel();
+
+            CurrentPage = Welcome;
+        }
+
+        public ICommand OpenProfilePage
+        {
+            get
+            {
+                return new RelayCommand(() => CurrentPage = Profile);
+            }
+        }
+
+
+    }
+}
