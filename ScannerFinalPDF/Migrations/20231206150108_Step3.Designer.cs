86650063fda047d262b60d7847c7a6acbdc65401
Mister Empty <mr.emptyboss@gmail.com>
2023-12-06 21:03:47 +0600

    возможность удаление записей из бд добавлено контекстное меню

--- ScannerFinalPDF/Migrations/20231206150108_Step3.Designer.cs
+++ ScannerFinalPDF/Migrations/20231206150108_Step3.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ScannerFinalPDF.Model.Data;
 
 namespace ScannerFinalPDF.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20231206150108_Step3")]
+    partial class Step3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- ScannerFinalPDF/Model/Data/User.cs
+++ ScannerFinalPDF/Model/Data/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,21 +18,19 @@ namespace ScannerFinalPDF.Model.Data
         public int PositionId { get; set; }
 
         public virtual Position Position { get; set; }
+        [NotMapped]
+        public Position UserPosition
+        {
+            get
+            {
+                return DataWorker.GetPosId(PositionId);
+            }
+        }
 
         public DateTime Date_create { get; set; }
         public DateTime Date_rozh { get; set; }
 
 
         public User() { }
-
-        public User(string login, string pass, string fio, int position, DateTime date_create, DateTime date_rozh)
-        {
-            Login = login;
-            Pass = pass;
-            Fio = fio;
-            PositionId = position;
-            Date_create = date_create;
-            Date_rozh = date_rozh;
-        }
     }
 }
